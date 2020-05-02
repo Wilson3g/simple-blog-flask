371e3b385ee0ee3aa3db6867568118cafc7fce26
wilson <wilson.sfj@outlook.com>
2020-05-02 15:17:40 -0300

    configurando chave estrangeira entre users e comments

--- microservice_1/app/model/Model.py
+++ microservice_1/app/model/Model.py
@@ -9,9 +9,10 @@ def configure(app):
 # Models
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    email = db.Column(db.String(100)) 
-    username = db.Column(db.String(50)) 
-    password = db.Column(db.String(100)) 
+    email = db.Column(db.String(100), nullable=False, unique=True) 
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    comment = db.relationship('comment')
     # is_admin = db.Column(db.String(10))
 
     def __init__(self, email, username, password):
@@ -23,10 +24,10 @@ class User(db.Model):
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    comment = db.Column(db.String(255)) 
+    comment = db.Column(db.String(255), nullable=False) 
     created_at = db.Column(db.String(50)) 
     updated_at = db.Column(db.String(100)) 
-    user_id = db.Column(db.String(100)) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
 
     def __init__(self, comment, created_at, updated_at, user_id):
         self.comment = comment
